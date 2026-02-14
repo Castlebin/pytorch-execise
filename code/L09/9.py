bfc2800778bf7927c7f295f6a910e41e513e4e18
Castlebin <Castlebin@gmail.com>
2026-02-14 23:05:35 +0800

    9. 多分类问题 (示例：识别 手写数字输入 (MNIST 数据集))

--- code/L09/9.py
+++ code/L09/9.py
@@ -1,24 +1,34 @@
+# 9. 多分类问题 (示例：识别 手写数字输入 (MNIST 数据集))
+
+import os
+
 import torch
-from torchvision import transforms
-from torchvision import datasets
-from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
+from dl_d2l.util import colab_util
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+
+base_data_dir = colab_util.get_base_data_dir()
+datasets_dir = os.path.join(base_data_dir, 'ML', 'Datasets')
+os.makedirs(datasets_dir, exist_ok=True)
+print(f'datasets dir: {datasets_dir}')
 
 # prepare dataset
-
 batch_size = 64
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])  # 归一化,均值和方差
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])  # 归一化,均值和方差
 
-train_dataset = datasets.MNIST(root='../dataset/mnist/', train=True, download=True, transform=transform)
+train_dataset = datasets.MNIST(root=datasets_dir, train=True, download=True, transform=transform)
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
-test_dataset = datasets.MNIST(root='../dataset/mnist/', train=False, download=True, transform=transform)
+test_dataset = datasets.MNIST(root=datasets_dir, train=False, download=True, transform=transform)
 test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 
 
 # design model using class
-
-
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -29,7 +39,7 @@ class Net(torch.nn.Module):
         self.l5 = torch.nn.Linear(64, 10)
 
     def forward(self, x):
-        x = x.view(-1, 784)  # -1其实就是自动获取mini_batch
+        x = x.view(-1, 784)  # -1 其实就是自动获取 mini_batch  (-1 表示自动计算该维度大小)
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
         x = F.relu(self.l3(x))
@@ -40,12 +50,15 @@ class Net(torch.nn.Module):
 model = Net()
 
 # construct loss and optimizer
-criterion = torch.nn.CrossEntropyLoss()
+loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
 
 # training cycle forward, backward, update
 def train(epoch):
+    # 训练模式
+    model.train()
+
     running_loss = 0.0
     for batch_idx, data in enumerate(train_loader, 0):
         # 获得一个批次的数据和标签
@@ -54,8 +67,8 @@ def train(epoch):
         # 获得模型预测结果(64, 10)
         outputs = model(inputs)
 
-        # 交叉熵代价函数outputs(64,10),target（64）
-        loss = criterion(outputs, target)
+        # 交叉熵代价函数 outputs(64,10), target（64）
+        loss = loss_fn(outputs, target)
 
         loss.backward()
 
@@ -70,17 +83,20 @@ def train(epoch):
 
 
 def test():
+    # 评估模式
+    model.eval()
+
     correct = 0
     total = 0
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
             outputs = model(images)
-            _, predicted = torch.max(outputs.data, dim=1)  # dim = 1 列是第0个维度，行是第1个维度
+            # 取最大的值作为预测分类
+            _, predicted = torch.max(outputs.data, dim=1)  # dim = 1 列是第 0 个维度，行是第 1 个维度
             total += labels.size(0)
             correct += (predicted == labels).sum().item()  # 张量之间的比较运算
     print('accuracy on test set: %d %% ' % (100 * correct / total))
-
 
 
 if __name__ == '__main__':
