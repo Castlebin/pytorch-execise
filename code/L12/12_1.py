9604c722ca2a152170300bdfe30feecfa1e2f31c
Castlebin <Castlebin@gmail.com>
2026-02-15 21:25:24 +0800

    12.1 RNN Cell

---
+++ code/L12/12_1.py
@@ -0,0 +1,45 @@
+"""
+12. 循环神经网络 (RNN) 基础篇
+
+1. RNN Cell
+"""
+import torch
+
+batch_size = 1
+seq_len = 3
+input_size = 4
+hidden_size = 2
+
+"""
+单个RNN
+即循环神经网络的单个部分
+多个部分组成的是RNN
+"""
+cell = torch.nn.RNNCell(input_size=input_size, hidden_size=hidden_size)
+
+# (seq, batch, features)
+"""
+生成标准正态分布的（均值为零，方差为一，的高斯白噪声）的随机数；
+使用torch.randn 生成（3, 1, 4）  --  张量
+生成为全为 0.的数，
+使用 torch.zeros 生成（1，2）  --  张量
+"""
+# 随机生产一份数据 （本示例只为观察 RNN Cell 计算过程中的维度）
+dataset = torch.randn(seq_len, batch_size, input_size)
+# 初始的 hidden 
+hidden = torch.zeros(batch_size, hidden_size)
+print(dataset)
+
+for idx, input in enumerate(dataset):
+    """
+    idx：第几次遍历
+    input：取出当前次数的数据，数据维度是（1，4）
+    cell（输入数据（batch，特征的向量维度），记忆体维度（batch，hidden的维度））
+    """
+    print('=' * 20, idx, '=' * 20)
+    hidden = cell(input, hidden)
+    print('outputs size: ', hidden.shape)
+    print(hidden)
+    
+    
+    
